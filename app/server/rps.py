718a4888cc5c38501f721a8a96e9a6f4258e0e82
Jonathan Wilson <jdw7@rice.edu>
2015-05-22 16:20:25 -0500

    Added login to restrict view of lobby and game urls

--- app/server/rps.py
+++ app/server/rps.py
@@ -1,33 +1,47 @@
 __author__ = 'jonathan'
 
-from flask import render_template, request, redirect, url_for, session
+from flask import render_template, request, redirect, url_for, session, flash
 from . import server
 from .. import socketio
+from functools import wraps
 
 users = []
 #routing
 @server.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        print "Got log in request from " + request.form['username']
-        print "with password " + request.form['password']
         session['name'] = request.form['username']
         if session['name'] not in users:
             users.append(session['name'])
-        return render_template('lobby.html', name=session['name'], users=users) #redirect(url_for('.lobby'))
+        return redirect(url_for('.lobby'))
     else:
         return render_template('login.html')
 
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'name' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You must log in to view this page')
+            return redirect(url_for('.login'))
+    return wrap
+
 
 @server.route('/lobby')
+@login_required
 def lobby():
     socketio.emit('status', {'msg': 'you have entered the room'}, broadcast=True)
     print "in lobby"
-    name = session['name']
+    if 'name' in session:
+        name = session['name']
+    else:
+        name = None
     return render_template('lobby.html', name=name, users=users)
 
 
 @server.route('/game', methods=['GET', 'POST'])
+@login_required
 def game():
     if request.method == 'POST':
         move = request.form['data']
