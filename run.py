f6ac9421b5020abfe7c93913f89952c466bb12c8
Jonathan Wilson <jdw7@rice.edu>
2015-05-21 19:21:53 -0500

    Changed procfile, call run in run.py

--- run.py
+++ run.py
@@ -5,5 +5,8 @@ from app import create_app, socketio
 
 app = create_app(True)
 
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
+
 if __name__ == '__main__':
     socketio.run(app)
