ba2760fde95db00ff4f7677f350424c42df067ee
Jonathan Wilson <jdw7@rice.edu>
2015-05-21 19:10:21 -0500

    Changed __init__.py's debug mode

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ socketio = SocketIO()
 def create_app(debug=False):
     """Create an application."""
     app = Flask(__name__)
-    app.debug = True
+    app.debug = debug
     app.config['SECRET_KEY'] = 'gjr39dkjn344_!67#'
 
     from .server import server as main_blueprint
